2ba462de81f2689b7088c1f78739e061617b3e76
haydaramru <haydaramrurevanda@mail.ugm.ac.id>
2023-11-28 23:58:50 +0700

    refactor: main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/src/features/authentication/screens/splash_screen/splash_screen.dart';
 import 'package:flutter_login/src/utils/theme/theme.dart';
 
-void main() {
-  runApp(const App());
-}
+void main() => runApp(const App());
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -14,58 +13,7 @@ class App extends StatelessWidget {
       theme: HAppTheme.lightTheme,
       darkTheme: HAppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: const AppHome(),
-    );
-  }
-}
-
-class AppHome extends StatelessWidget {
-  const AppHome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        title: const Text(".appable/"),
-        leading: const Icon(Icons.ondemand_video),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add_shopping_cart_outlined),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ListView(
-          children: [
-            Text(
-              "Heading",
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              "Sub-heading",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Text(
-              "Paragraph",
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Elevated Button"),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text("Outlined Button"),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(20.0),
-              child:
-                  Image(image: AssetImage("assets/images/simaster_icon.png")),
-            )
-          ],
-        ),
-      ),
+      home: const SplashScreen(),
     );
   }
 }
